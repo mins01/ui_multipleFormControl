8d52ed43b9ef81cd99aee446bb86b239233407f5
mins01 <mins01.lycos.co.kr@gmail.com>
2023-10-20 17:14:18 +0900

    first - deving

---
+++ MultipleFormControl.js
@@ -0,0 +1,41 @@
+"use strict";
+
+class MultipleFormControl{
+    static instance = null;
+    static getInstance(){
+        if(!this.instance){
+
+        }
+        return this.instance;
+    }
+    static activate(){
+    }
+    static deactivate(){
+    }
+
+
+
+
+
+    constructor(){
+
+    }
+
+
+    /**
+     * form-control element 가져오기
+     */
+    fcElement(target){
+
+    }
+    cloneFcElement(){
+
+    }
+    
+
+
+
+
+
+
+}
